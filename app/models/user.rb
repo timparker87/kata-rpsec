ad622d472617288534f5b002c2b697d8f24eb921
tim <tim@resident.ly>
2019-03-01 10:08:42 +0000

    exercise 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+
+  attr_accessor :year_of_birth
+
+  def age
+    return nil if @year_of_birth == nil 
+    Date.current.year - @year_of_birth.to_i
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  subject do
+    User.new
+  end
+  
+  context 'when the year of birth is 1991' do
+    before do
+      subject.year_of_birth = 1991
+    end
+
+    describe '#age' do
+      before do
+        subject.year_of_birth = 1991
+      end
+
+      it 'returns the users age  given year_of_birth' do      
+        expect(subject.age).to eq 28
+      end
+    end
+  end
+
+  describe '#age' do   
+    before do
+    end
+
+    it 'handles a string year_of_birth' do 
+      user = User.new(year_of_birth: '1991')
+      expect(user.age).to eq 28
+    end 
+
+    it 'handles null values' do 
+      user = User.new(year_of_birth: nil)
+      expect(user.age).to eq nil
+    end 
+
+  end
 end
